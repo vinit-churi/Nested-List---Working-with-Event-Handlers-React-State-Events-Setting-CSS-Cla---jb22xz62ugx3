f589255ab53f3f50c7df59712a2aab74d214e2a2
vinit-churi <vinitchuri@hotmail.com>
2023-02-23 03:27:44 +0530

    a

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ function App() {
                                 key={uniqueID()}
                                 name={state.name}
                                 cities={state.cities}
-                                id={`state-${index + 1}`}
+                                id={`state${index + 1}`}
                             />
                         );
                     })}
